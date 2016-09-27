fa665d1fb2d44d48a24c8adecd4c8eebf49859a1
rethinkflash <ken@rethinkcanada.com>
2016-09-27 12:56:18 -0700

    doing a quick servo test

--- main.cpp
+++ main.cpp
@@ -45,50 +45,53 @@ int main(int argc, char* argv[]) {
 		signal(SIGINT, [](int param){ shouldRun = false; });
 
 		// Initialize wiringPi library.
-		wiringPiSetupInt = wiringPiSetup();
+		if (wiringPiSetup () == -1)
+			exit (1) ;
 		pinMode(SERVO_PIN, OUTPUT);
 		digitalWrite(SERVO_PIN, LOW);
 
 		softPwmCreate(SERVO_PIN, 0, 200);
 		softPwmWrite(SERVO_PIN, 180);
 
-		// Load alarm raw audio.
-		ifstream input(ALARM_FILE, ios::in | ios::binary);
-		input.seekg (0, input.end);
-		size_t length = input.tellg();
-		input.seekg (0, input.beg);
-		vector<uint8_t> alarm(length);
-		input.read((char*)alarm.data(), length);
+		cout << "Servo done" << endl;
 
-		// Initialize audio sink and source.
-		AlsaSink sink;
-		sink.open(PLAYBACK_HW, 44100, 1, SND_PCM_FORMAT_S16_LE);
-		AlsaSource source;
-		source.open(RECORD_HW, 16000, 1, SND_PCM_FORMAT_S16_LE);
-
-		// Initialize keyword spotter.
-		PocketSphinxKWS spotter;
-		spotter.initialize(PocketSphinxKWS::parseConfig(argc, argv), KEYWORD_FILE);
-
-		// Initialize main logic.
-		SajeMonitor monitor(8000, &source, &sink, &spotter, &alarm, SERVO_PIN);
-
-		cout << "Listening... (press Ctrl-C to stop)" << endl;
-
-		while (shouldRun) {
-			// Check quite mode switch and update state.
-			// Update main logic state.
-			monitor.update();
-		}
+		// // Load alarm raw audio.
+		// ifstream input(ALARM_FILE, ios::in | ios::binary);
+		// input.seekg (0, input.end);
+		// size_t length = input.tellg();
+		// input.seekg (0, input.beg);
+		// vector<uint8_t> alarm(length);
+		// input.read((char*)alarm.data(), length);
+		//
+		// // Initialize audio sink and source.
+		// AlsaSink sink;
+		// sink.open(PLAYBACK_HW, 44100, 1, SND_PCM_FORMAT_S16_LE);
+		// AlsaSource source;
+		// source.open(RECORD_HW, 16000, 1, SND_PCM_FORMAT_S16_LE);
+		//
+		// // Initialize keyword spotter.
+		// PocketSphinxKWS spotter;
+		// spotter.initialize(PocketSphinxKWS::parseConfig(argc, argv), KEYWORD_FILE);
+		//
+		// // Initialize main logic.
+		// SajeMonitor monitor(8000, &source, &sink, &spotter, &alarm, SERVO_PIN);
+		//
+		// cout << "Listening... (press Ctrl-C to stop)" << endl;
+		//
+		// while (shouldRun) {
+		// 	// Check quite mode switch and update state.
+		// 	// Update main logic state.
+		// 	monitor.update();
+		// }
 	}
-	catch (AlsaError ex) {
-		cerr << "ALSA ERROR " << ex.message << " (" << ex.code << ") while calling: " << ex.what() << endl;
-		return 1;
-	}
-	catch (exception ex) {
-		cerr << "ERROR: " << ex.what() << endl;
-		return 1;
-	}
+	// catch (AlsaError ex) {
+	// 	cerr << "ALSA ERROR " << ex.message << " (" << ex.code << ") while calling: " << ex.what() << endl;
+	// 	return 1;
+	// }
+	// catch (exception ex) {
+	// 	cerr << "ERROR: " << ex.what() << endl;
+	// 	return 1;
+	// }
 
 	return 0;
 }
