9dc155d1e1d517b1f0e4c79ced2b46cf8699a28f
rethinkflash <ken@rethinkcanada.com>
2016-09-29 14:37:36 -0700

    trying another example

--- main.cpp
+++ main.cpp
@@ -49,8 +49,11 @@ int main(int argc, char* argv[]) {
 		if (wiringPiSetup () == -1)
 			exit (1) ;
 
-		softPwmCreate(0, 0,100);
-		softPwmWrite(0, 20);
+		pinMode(1, PWM_OUTPUT);
+		pwmSetMode(PWM_MODE_MS);
+		pwmSetClock(384); //clock at 50kHz (20us tick)
+		pwmSetRange(1000); //range at 1000 ticks (20ms)
+		pwmWrite(1, 75);  //theretically 50 (1ms) to 100 (2ms) on my servo 30-130 works ok
 		cout << "Servo written" << endl;
 		delay (3000);
 		// digitalWrite(SERVO_PIN, LOW);
