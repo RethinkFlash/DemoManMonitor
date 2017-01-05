195f77d479cf548c4f158416cec22b346bbdaba9
rethinkflash <ken@rethinkcanada.com>
2017-01-05 10:49:12 -0800

    updated the run command

--- run_main.sh
+++ run_main.sh
@@ -2,4 +2,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 export LD_LIBRARY_PATH=/usr/local/lib
-${DIR}/EscentsMonitor -hmm ${DIR}/models/hub4wsj_sc_8k_adapted/ -dict ${DIR}/models/cmu07a.dic -lm ${DIR}/models/wsj0vp.5000.DMP -logfn /dev/null
+## old version
+#${DIR}/EscentsMonitor -hmm ${DIR}/models/hub4wsj_sc_8k_adapted/ -dict ${DIR}/models/cmu07a.dic -lm ${DIR}/models/wsj0vp.5000.DMP -logfn /dev/null
+${DIR}/EscentsMonitor -hmm ${DIR}/models/en-us-new-swears/ -dict ${DIR}/models/cmudict-en-us.dic -lm ${DIR}/models/wsj0vp.5000.DMP -logfn /dev/null
